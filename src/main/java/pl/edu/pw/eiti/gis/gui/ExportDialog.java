c705bdb48717287db403c3cf6156d763b0559334
psadlo <psadlo@kartgis.com.pl>
2015-12-12 12:48:57 +0100

    export window components as class fields (to access them from outside)

--- src/main/java/pl/edu/pw/eiti/gis/gui/ExportDialog.java
+++ src/main/java/pl/edu/pw/eiti/gis/gui/ExportDialog.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 public class ExportDialog extends JDialog {
 
     private final MainWindow mainWindow;
+    private final JRadioButton textExport = new JRadioButton("macierz tekstowa");
+    private final JRadioButton wordExport = new JRadioButton("macierz w formacie MathML");
+    private final JRadioButton imageExport = new JRadioButton("rysunek macierzy");
+    private final JRadioButton graphImageExport = new JRadioButton("rysunek grafu");
+    private final JRadioButton neighbourMatrix = new JRadioButton("macierz sąsiedztwa");
+    private final JRadioButton fullIncidenceMatrix = new JRadioButton("pełna macierz incydencji");
+    private final JRadioButton weightMatrix = new JRadioButton("macierz wag");
+
+    private final JButton exportBtn = new JButton("Kopiuj do schowka");
 
     public ExportDialog(MainWindow mainWindow) {
         super(mainWindow, "Eksport grafu", true);
@@ -31,70 +40,52 @@ public class ExportDialog extends JDialog {
     private JPanel getOptionsPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.add(getMatrixOptions());
-        panel.add(getTypeOptions());
-        panel.add(getLineBreakOptions());
+        panel.add(getExportTypeOptions());
+        panel.add(getExportMatrixOptions());
         return panel;
     }
 
-    private JPanel getMatrixOptions() {
+    private JPanel getExportTypeOptions() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(textExport);
+        panel.add(wordExport);
+        panel.add(imageExport);
+        panel.add(graphImageExport);
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(textExport);
+        group.add(wordExport);
+        group.add(imageExport);
+        group.add(graphImageExport);
+
+        textExport.setSelected(true);
+
+        return panel;
+    }
+
+    private JPanel getExportMatrixOptions() {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        ButtonGroup group = new ButtonGroup();
-        addRadioButton(panel, group, "macierz sąsiedztwa").setSelected(true);
-        addRadioButton(panel, group, "pełna macierz incydencji");
-        addRadioButton(panel, group, "macierz wag");
-
-        return panel;
-    }
-
-    private JPanel getTypeOptions() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(neighbourMatrix);
+        panel.add(fullIncidenceMatrix);
+        panel.add(weightMatrix);
 
         ButtonGroup group = new ButtonGroup();
-        addRadioButton(panel, group, "tekst").setSelected(true);
-        addRadioButton(panel, group, "obraz");
-        addRadioButton(panel, group, "Word");
+        group.add(neighbourMatrix);
+        group.add(fullIncidenceMatrix);
+        group.add(weightMatrix);
+
+        neighbourMatrix.setSelected(true);
 
         return panel;
-    }
-
-    private JPanel getLineBreakOptions() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        ButtonGroup group = new ButtonGroup();
-        addRadioButton(panel, group, "Windows (CR+LF)").setSelected(true);
-        addRadioButton(panel, group, "Unix (LF)");
-        addRadioButton(panel, group, "MacOS (CR)");
-
-        return panel;
-    }
-
-    private JRadioButton addRadioButton(JPanel panel, ButtonGroup group, String label) {
-        JRadioButton btn = new JRadioButton(label);
-        group.add(btn);
-        panel.add(btn);
-        return btn;
     }
 
     private JPanel getButtonsPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.add(getCopyToClipboardButton());
-        panel.add(getSaveToFileButton());
+        panel.add(exportBtn);
         return panel;
-    }
-
-    private JButton getCopyToClipboardButton() {
-        JButton btn = new JButton("Kopiuj do schowka");
-        return btn;
-    }
-
-    private JButton getSaveToFileButton() {
-        JButton btn = new JButton("Zapisz do pliku");
-        return btn;
     }
 }
